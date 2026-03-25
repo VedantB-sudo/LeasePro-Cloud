b2920ac7facff84e9836124e2751ebc221c8b8cd
VedantB-sudo <vedantbandarkar@gmail.com>
2026-03-25 20:37:39 +0000

    oiehgohiewge

--- core/tests.py
+++ core/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+class SimpleTest(TestCase):
+    """
+    A placeholder test class to verify the test suite and resolve Pylint warnings.
+    """
+    def test_basic_addition(self):
+        """
+        A simple functional test to ensure the environment is configured correctly.
+        """
+        self.assertEqual(1 + 1, 2)
